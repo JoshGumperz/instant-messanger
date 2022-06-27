246870eda523f02432ec755634da04c46787085c
Josh Gumperz <joshg3023@gmail.com>
2022-06-26 19:58:32 -0700

    created initial navbar component and setup initial styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import { Switch, Route } from 'react-router-dom';
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+// import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
-import TestRoute from './components/TestRoute/TestRoute';
+import Navbar from './components/Navbar/Navbar';
+// import TestRoute from './components/TestRoute/TestRoute';
 import './App.css';
 
 
 function App() {
   return (
-    <Switch>
-        <Route exact path={'/'} component={Home} />
-        <Route exact path={'/login'} component={Login}/>
-    </Switch>
+    <div>
+      <Navbar/>
+      <Switch>
+          <Route exact path={'/'} component={Home} />
+          <Route exact path={'/login'} component={Login}/>
+      </Switch>
+    </div>
   );
 }
 
