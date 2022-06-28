640bc046fcb1ce6275d4c9671fe816171801b09c
Josh Gumperz <joshg3023@gmail.com>
2022-06-27 17:21:08 -0700

    finished initial mockup for navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { IoHome, IoPersonCircle } from 'react-icons/io5'
+import { Link } from 'react-router-dom'
 import "./Navbar.css"
 
 
-function Navbar({ toggleSidebar }) {
+function Navbar() {
   return (
     <>
         <header className='navbar-nav'>
-            nav
+           <div className='navbar-navContainer'>
+              <h3 className='navbar-navLogo'>InstantMessenger</h3>
+              <ul className='navbar-navList'>
+                <li className='navbar-navItem'>
+                  <Link className='navbar-navLink'to={'/'}><IoHome className='navbar-navIcon navbar-homeIcon'/></Link>
+                </li>
+                <li className='navbar-navItem'>
+                  <Link className='navbar-navLink' to={'/login'}><IoPersonCircle className='navbar-navIcon navbar-profileIcon'/></Link>
+                </li>
+              </ul>
+           </div>
         </header>
     </>
   )
